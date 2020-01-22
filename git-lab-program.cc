8bcf4a5da2a1f6881d4c54525ba3f7613d967bf5
Sophia Brown <sb911718@ohio.edu>
2020-01-22 18:29:48 -0500

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Sophia Brown>
+ *        Date: <January 22, 2020>
+ * Description: Hello Git display
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
